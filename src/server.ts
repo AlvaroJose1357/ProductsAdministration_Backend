3c268e71391c3c10cbdffcfb63df5a82111c200a
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-07 23:23:43 -0500

    feat: add router for API products and refactor server to use it

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/products", router);
 
 export default app;
