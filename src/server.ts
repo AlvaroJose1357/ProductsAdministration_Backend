0cb6a23e890f8a9f46468e99cb10076a83176f8b
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-11 02:48:43 -0500

    refactor: move router to a new directory and update import paths

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import router from "./router";
+import router from "./routes/router";
 import { connectBD } from "./config/BD";
 const app = express();
 // conexion a la base de datos
