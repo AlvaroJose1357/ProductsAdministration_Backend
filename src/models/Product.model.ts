2b2b2a76a8b0ae4d202e6c96d83e7ee0d554cd16
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-11 03:16:54 -0500

    docs(types data) adding the url to visualize the data types and also an example

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -3,6 +3,28 @@ import { Table, Column, Model, DataType } from "sequelize-typescript";
 @Table({
   tableName: "products",
 })
+// para visualizar los diferentes tipos de datos disponibles en sequelize https://sequelize.org/docs/v7/models/data-types/
+
+// en nuestro caso solamente llevara el nombre, price, y disponibilidad
+export default class Product extends Model {
+  @Column({
+    type: DataType.STRING(100), // para el tipo de dato
+    allowNull: false, // para que no sea nulo
+  })
+  name: string;
+
+  @Column({
+    type: DataType.FLOAT(7, 2),
+    allowNull: false,
+  })
+  price: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+  })
+  availavility: boolean;
+}
 
 // export class Product extends Model {
 //   @Column({
@@ -36,24 +58,3 @@ import { Table, Column, Model, DataType } from "sequelize-typescript";
 //   })
 //   stock: number;
 // }
-
-// en nuestro caso solamente llevara el nombre, price, y disponibilidad
-export default class Product extends Model {
-  @Column({
-    type: DataType.STRING(100), // para el tipo de dato
-    allowNull: false, // para que no sea nulo
-  })
-  name: string;
-
-  @Column({
-    type: DataType.FLOAT(7, 2),
-    allowNull: false,
-  })
-  price: number;
-
-  @Column({
-    type: DataType.BOOLEAN,
-    allowNull: false,
-  })
-  availavility: boolean;
-}
