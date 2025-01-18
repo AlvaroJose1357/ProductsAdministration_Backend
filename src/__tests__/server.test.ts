25ab7b61c5731c94b1cdb263451a2c6e8eae795b
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-17 22:07:27 -0500

    test: enhance API response tests with additional assertions

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -12,9 +12,13 @@ import app from "../server";
 describe("GET /api", () => {
   it("should send back a JSON response", async () => {
     const res = await request(app).get("/api");
+    //? las que deben de ser
     expect(res.status).toBe(200);
     expect(res.headers["content-type"]).toMatch(/json/);
     // expect(res.body).toEqual({ message: "API con express y typescript" });
     expect(res.body.message).toBe("API con express y typescript");
+    //? las que no deben de ser
+    expect(res.status).not.toBe(404);
+    expect(res.body.message).not.toBe("API con express y typescript 2");
   });
 });
